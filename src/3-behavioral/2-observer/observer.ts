27785d8a85a253e8a86ffb54b0e171bc5463c6f1
Alberto Basalo <albertobasalo@hotmail.com>
2023-02-14 12:06:31 +0100

    feat: command

--- src/3-behavioral/2-observer/observer.ts
+++ src/3-behavioral/2-observer/observer.ts
@@ -1,16 +1,16 @@
 // * ✅ Observer solution
 
-// *  😏 the observer contract is a function used as a listener or callback
+// * 😏 the observer contract is a function used as a listener or callback
 type Observer = (data: object) => void;
 
-// *  😏 the observable contract is a set of methods to subscribe, unsubscribe and publish events
+// * 😏 the observable contract is a set of methods to subscribe, unsubscribe and publish events
 interface Observable {
   subscribe(eventName: string, observer: Observer): void;
   unsubscribe(eventName: string, observer: Observer): void;
   publish(eventName: string, eventArgs: object): void;
 }
 
-// *  😏 the event bus implements the observable contract
+// * 😏 the event bus implements the observable contract
 export class EventBus implements Observable {
   private subscriptions: Map<string, Observer[]> = new Map();
 
